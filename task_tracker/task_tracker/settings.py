26dce5155b03e4a94971feaa2a4ce92a9ea19e4e
oswaty <oswafy@gmail.com>
2024-08-26 15:57:47 +0300

    Creating models

--- task_tracker/task_tracker/settings.py
+++ task_tracker/task_tracker/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Application definition
 
 INSTALLED_APPS = [
+    'tasks',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
