add70300b22ce87d01b13fd91daa00e8adbc4d7a
oswaty <oswafy@gmail.com>
2024-08-27 13:52:58 +0300

    all functionality working fine

--- task_tracker/tasks/urls.py
+++ task_tracker/tasks/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('register',views.register, name='register'),
     path('login',views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('task_list',views.task_list,name="task_list")
+    path('task_list',views.task_list,name="task_list"),
+    path('tasks/<int:pk>',views.task_detail,name="task_detail"),
+    path('task_create',views.task_create,name='task_create')
 
  ]
  
--- task_tracker/tasks/views.py
+++ task_tracker/tasks/views.py
@@ -4,7 +4,14 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django import forms
 # Create your views here.
+
+class NewTaskForm(forms.Form):
+    title=forms.CharField(label="New Task")
+    description=forms.CharField(label="Description")
+    due_date=forms.DateField()
+
 
 def index(request):
     return render(request,'tasks/index.html')
@@ -17,7 +24,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('task_list')
+            return redirect('tasks:task_list')
     else:
         form = UserCreationForm()
     return render(request, 'tasks/register.html', {'form': form})
@@ -45,12 +52,20 @@ def task_list(request):
 @login_required
 def task_create(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        due_date = request.POST['due_date']
-        task = Task.objects.create(title=title, description=description, due_date=due_date, user=request.user)
-        return redirect('task_list')
-    return render(request, 'tasks/task_create.html')
+        form=NewTaskForm(request.POST)
+        if form.is_valid():
+            title=form.cleaned_data["title"]
+            description = form.cleaned_data['description']
+            due_date=form.cleaned_data['due_date']
+            task = Task.objects.create(title=title, description=description, due_date=due_date, user=request.user)
+            return redirect('tasks:task_list')
+        else:
+             return render(request,"tasks/task_create.html",{
+               "form":form 
+            }) 
+    return render(request, 'tasks/task_create.html',{
+        "form":NewTaskForm()
+    })
 
 @login_required
 def task_detail(request, pk):
